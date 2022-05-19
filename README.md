20e9058638ccfe0926c4c392ec065df22be00a4b
mvishu405 <mvishu405@gmail.com>
2022-05-19 09:54:08 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-5z3tap
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-5z3tap)
---
+++ index.js
@@ -0,0 +1,4 @@
+// Import stylesheets
+import './style.css';
+
+const cnv = document.getElementById('cnv');
